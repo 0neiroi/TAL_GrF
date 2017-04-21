3e7761a71698a9370526414e0546e26040a089b3
Jamy4000 <arnaudbriche1994@gmail.com>
2017-04-21 21:30:11 +0200

    Traitement XML, récup NNP en F et suppression des doublons
    Mise en place d'un script avec plusieurs méthodes, notamment pour parcourir le fichier XML et récupérer les NNP commençant par un F. 
    Ajout également d'une fonction pour virer les doublons, c'est plus propre quand même

---
+++ Script/DetectionEntitesNommees.py
@@ -0,0 +1,51 @@
+import nltk
+from nltk import tag
+from xml.dom import minidom
+import xml.etree.ElementTree as etree
+
+xmldoc = open('Wikipedia_F_only_Tokenizé par CoreNLP.xml','r', encoding = 'utf-8')
+tree = etree.parse(xmldoc)
+root = tree.getroot()
+
+def getNNPAttribs():
+	listReturned = []
+	for document in root:
+		for sentences in document:
+			for sentence in sentences:
+				for tokens in sentence:
+					for token in tokens:
+						tag = token.find('POS').text
+						if (tag == 'NNP'):
+							name = token.find('word').text
+							listReturned.append(name)
+
+	return listReturned
+
+def getFNNP(listOfNPP):
+	listReturned = []
+	for i in range(len(listOfNPP)):
+		if (listOfNPP[i][:1].lower() == "f"):
+			listReturned.append(listOfNPP[i])
+	return listReturned
+
+def checkDoublon(listOfName):
+	listReturned = []
+	for i in range(len(listOfName)):
+		if (lookInTheReturnedList(listReturned, listOfName[i])):
+			listReturned.append(listOfName[i])
+	return listReturned
+
+def lookInTheReturnedList(actualList, name):
+	for i in range(len(actualList)):
+		if (actualList[i].lower() == name.lower()):
+			return False
+	return True
+
+def printAList(theList):
+	for i in range(len(theList)):
+		print(theList[i])
+
+nnp = getNNPAttribs()
+list_Of_F_Names = getFNNP(nnp)
+list_Of_F_Names = checkDoublon(list_Of_F_Names)
+printAList(list_Of_F_Names)
